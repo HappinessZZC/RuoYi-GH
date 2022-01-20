ff8383ee0094f365d1b73d3227a431e7b5d8292a
Zzc <572196146@qq.com>
2022-01-20 08:18:10 +0800

    vue.login

---
+++ ruoyi-ui/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    123
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login {
+  background-color: bisque;
+}
+</style>
